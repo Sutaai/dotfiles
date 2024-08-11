141d3712505f0a7a5b4af6e24ad0dd71941ee74b
Sutaai <sutaai@proton.me>
2024-08-12 01:56:14 +0200

    feat: initial config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Starship prompt
+    starship init fish | source
+end
+
+# Chezmoi dotfile manager
+fish_add_path $HOME/.local/bin
+
+# Editor
+set EDITOR "zed --wait"
