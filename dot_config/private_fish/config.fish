249ff9885461afac21d6ae2be63fe15200cf6055
Sutaai <sutaai@proton.me>
2024-08-31 06:28:35 +0200

    feat: thefuck, cargo, add user's bin, zed settings

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    echo 
+    echo
 end
 
 if status is-interactive
@@ -7,12 +7,17 @@ if status is-interactive
     starship init fish | source
 end
 
-# Chezmoi dotfile manager
-fish_add_path $HOME/.local/bin
-
 # Editor
 set EDITOR "zed --wait"
 abbr -a -- l 'ls -alhv'
 
+# Chezmoi dotfile manager & whatever else
+fish_add_path $HOME/.local/bin
+
 # Alias for "thefuck" package
 thefuck --alias | source
+
+# Rust cargo 'n all
+if test -d "$HOME/.cargo"
+   source "$HOME/.cargo/env.fish" 
+end
